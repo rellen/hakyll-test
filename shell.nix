b417c8e6dda5509de9e130b79606f370f265ac1e
Robert Ellen <robert@alembic.com.au>
2025-02-19 21:37:30 +1000

    cleanup shell file

--- shell.nix
+++ shell.nix
@@ -9,8 +9,4 @@ in pkgs.mkShell rec {
     [ ghc zlib ] ++ optional stdenv.isLinux inotify-tools
     ++ optionals stdenv.isDarwin
     (with darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices Cocoa ]);
-
-  shellHook = ''
-    # this allows mix to work on the local directory
-  '';
 }
